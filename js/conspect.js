6a524f2ef0b5b1461219bd7d76d8fc9d918ae410
Filatov Dmytro <dmitrovfilatov@gmail.com>
2023-05-30 22:49:54 +0300

    8 module
     8 module

---
+++ js/conspect.js
@@ -0,0 +1,5 @@
+
+
+const result = _.toLower('--Foo-Bar--');
+console.log(result); // 5
+
--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-
-
-const itemElm = document.querySelectorAll(".item");
-console.dir(itemElm);
-console.log(`Number of categories: ${itemElm.length}`);
-itemElm.forEach((item) => {
-    console.log(`Category: ${item.firstElementChild.innerText}`);
-     console.dir(`Elements: ${item.lastElementChild.children.length}`);
-});
